846f561132c691f3daedfc04d3cbd457ae8ad3f6
christopherjneelankavil <christopherneelankavil@gmail.com>
2023-12-01 00:40:49 +0530

    first commit

---
+++ bubbleSort.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+void bubbleSort(int arr[],int size);
+void printArray(int arr[],int size);
+int main(){
+    int n;
+
+    //read the size of the array
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+
+    //reading array elements
+    printf("Enter the elements: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    //printing array elements
+    printf("The array entered is: \n");
+    printArray(arr,n);
+    //calling bubble sort function
+    bubbleSort(arr,n);
+
+    //printing sorted array
+    printf("The sorted array is: \n");
+    printArray(arr,n);
+    return 0;
+}
+
+void printArray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int arr[],int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+// Output
+// Enter the size of the array: 5
+// Enter the elements:
+// 16
+// 1
+// 456
+// 2
+// 6
+// The array entered is:
+// 16 1 456 2 6
+// The sorted array is:
+// 1 2 6 16 456
+
+// Algorithm BubbleSort
+// Input: An array arr of size n
+// Output: Sorted array arr
+
+// 1. Start
+// 2. Read n (size of the array) from the user
+// 3. Declare an array arr of size n
+// 4. Read n elements into the array arr
+// 5. Print "The array entered is:"
+// 6. Call the printArray function with parameters arr and n
+// 7. Call the bubbleSort function with parameters arr and n
+// 8. Print "The sorted array is:"
+// 9. Call the printArray function with parameters arr and n
+// 10. End
+
+// Algorithm printArray
+// Input: An array arr of size n
+// 1. For i = 0 to n-1
+//     a. Print arr[i] followed by a space
+// 2. Print a newline character
+// 3. End
+
+// Algorithm bubbleSort
+// Input: An array arr of size n
+// 1. For i = 0 to n-2
+//     a. For j = 0 to n-i-2
+//         i. If arr[j] > arr[j+1]
+//             A. Swap arr[j] and arr[j+1]
+// 2. End
+// 3. End
+
---
+++ selectionSort.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+void selectionSort(int arr[],int size);
+void printArray(int arr[],int size);
+int main(){
+    int n;
+
+    //read the size of the array
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+
+    //reading array elements
+    printf("Enter the elements: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    //printing array elements
+    printf("The array entered is: \n");
+    printArray(arr,n);
+    //calling selectionsort sort function
+    selectionSort(arr,n);
+
+    //printing sorted array
+    printf("The sorted array is: \n");
+    printArray(arr,n);
+    return 0;
+}
+
+void printArray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+void selectionSort(int arr[],int size){
+    for(int i=0;i<=size-2;i++){
+        for(int j=i+1;j<=size-1;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
+
+// Output
+// Enter the size of the array: 5
+// Enter the elements:
+// 12
+// 10
+// 8
+// 90
+// 2
+// The array entered is:
+// 12 10 8 90 2
+// The sorted array is:
+// 2 8 10 12 90
+
+// Algorithm SelectionSort
+// Input: An array arr of size n
+// Output: Sorted array arr
+
+// 1. Start
+// 2. Read n (size of the array) from the user
+// 3. Declare an array arr of size n
+// 4. Read n elements into the array arr
+// 5. Print "The array entered is:"
+// 6. Call the printArray function with parameters arr and n
+// 7. Call the selectionSort function with parameters arr and n
+// 8. Print "The sorted array is:"
+// 9. Call the printArray function with parameters arr and n
+// 10. End
+
+// Algorithm printArray
+// Input: An array arr of size n
+// 1. For i = 0 to n-1
+//     a. Print arr[i] followed by a space
+// 2. Print a newline character
+// 3. End
+
+// Algorithm selectionSort
+// Input: An array arr of size n
+// 1. For i = 0 to n-2
+//     a. For j = i+1 to n-1
+//         i. If arr[i] > arr[j]
+//             A. Swap arr[i] and arr[j]
+// 2. End
+// 3. End
